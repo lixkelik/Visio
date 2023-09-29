cdbd3f69db3efaf27e12b1da89fe1fa2dfbddbd0
lixkelik <lixkelik@gmail.com>
2023-09-30 01:38:04 +0700

    remove safeArea

--- lib/view/onboarding/onboarding_page.dart
+++ lib/view/onboarding/onboarding_page.dart
@@ -37,85 +37,83 @@ class _OnboardingPageState extends State<OnboardingPage> {
         return shouldExit ?? false;
       },
       child: Scaffold(
-        body: SafeArea(
-          child: AnimatedContainer(
-            duration: const Duration(milliseconds: 300),
-            color: (_pageIndex == 0) ? lightPink : (_pageIndex == 1) ? lightPurple : (_pageIndex == 2) ? lightGreen : (_pageIndex == 3) ? lightBlue : white,
-            child: Column(
-              children: [
-                Expanded(
-                  child: PageView.builder(
-                    physics: const BouncingScrollPhysics(),
-                    controller: _pageController,
-                    itemCount: onboardData.length,
-                    onPageChanged: (index){
-                      setState(() {
-                        _pageIndex = index;
-                      });
-                    },
-                    itemBuilder: (context, index) {
-                      return OnboardController(
-                        image: onboardData[index].image,
-                        title: onboardData[index].title,
-                        description: onboardData[index].description,
-                        
-                      );
-                    }
-                  ),
+        body: AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          color: (_pageIndex == 0) ? lightPink : (_pageIndex == 1) ? lightPurple : (_pageIndex == 2) ? lightGreen : (_pageIndex == 3) ? lightBlue : white,
+          child: Column(
+            children: [
+              Expanded(
+                child: PageView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  controller: _pageController,
+                  itemCount: onboardData.length,
+                  onPageChanged: (index){
+                    setState(() {
+                      _pageIndex = index;
+                    });
+                  },
+                  itemBuilder: (context, index) {
+                    return OnboardController(
+                      image: onboardData[index].image,
+                      title: onboardData[index].title,
+                      description: onboardData[index].description,
+                      
+                    );
+                  }
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Row(
-                    children: [
-                      ...List.generate(
-                        onboardData.length,
-                        (index) => Padding(
-                          padding: const EdgeInsets.only(right: 4),
-                          child: DotIndicator(
-                            isActive: index == _pageIndex,
-                          ),
-                        )
-                      ),
-                      const Spacer(),
-                      ElevatedButton(
-                        onPressed: () {
-                          if(_pageIndex == onboardData.length-1){
-                            Navigator.pushReplacement(
-                              context, MaterialPageRoute(builder: (context) => const LoginPage())
-                            );
-                          }else{
-                            _pageController.nextPage(duration: const Duration(milliseconds: 500), curve: Curves.ease);
-                          }
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: appOrange,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Row(
+                  children: [
+                    ...List.generate(
+                      onboardData.length,
+                      (index) => Padding(
+                        padding: const EdgeInsets.only(right: 4),
+                        child: DotIndicator(
+                          isActive: index == _pageIndex,
                         ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            AnimatedOpacity(
-                              duration: const Duration(milliseconds: 600),
-                              opacity: (_pageIndex == onboardData.length-1)? 1 : 0,
-                              child: Text(
-                                (_pageIndex == onboardData.length-1) ? "Start" : "",
-                                style: styleWSB20,
-                              ),
+                      )
+                    ),
+                    const Spacer(),
+                    ElevatedButton(
+                      onPressed: () {
+                        if(_pageIndex == onboardData.length-1){
+                          Navigator.pushReplacement(
+                            context, MaterialPageRoute(builder: (context) => const LoginPage())
+                          );
+                        }else{
+                          _pageController.nextPage(duration: const Duration(milliseconds: 500), curve: Curves.ease);
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: appOrange,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          AnimatedOpacity(
+                            duration: const Duration(milliseconds: 600),
+                            opacity: (_pageIndex == onboardData.length-1)? 1 : 0,
+                            child: Text(
+                              (_pageIndex == onboardData.length-1) ? "Start" : "",
+                              style: styleWSB20,
                             ),
-                            const SizedBox(width: 5,),
-                            const Icon(Icons.arrow_forward_ios_rounded),
-                          ],
-                        ),
+                          ),
+                          const SizedBox(width: 5,),
+                          const Icon(Icons.arrow_forward_ios_rounded),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                
-              ],
-            ),
+              ),
+              
+            ],
           ),
         ),
       ),
