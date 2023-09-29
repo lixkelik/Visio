07525dbd7b8bf8df98539a1427560c75b6e79f6a
lixkelik <lixkelik@gmail.com>
2023-09-30 02:17:25 +0700

    fix minor bug where cannot exit app

--- lib/view/authentication/register_page.dart
+++ lib/view/authentication/register_page.dart
@@ -33,7 +33,7 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async{
-        final shouldExit = showExitDialog(context);
+        final shouldExit = await showExitDialog(context);
         return shouldExit ?? false;
       },
       child: Scaffold(
