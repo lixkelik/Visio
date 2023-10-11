5ba5853c486d64c5b34c849c20093282df684109
Natasha Grace <natashagradika@gmail.com>
2023-10-11 15:22:48 +0700

    braille

---
+++ lib/view/impaired/braille_introduction_page.dart
@@ -0,0 +1,38 @@
+import "package:visio/constant/constant_builder.dart";
+
+class BrailleIntroductionPage extends StatelessWidget {
+  const BrailleIntroductionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: lightPink,
+      appBar: AppBar(
+        leading: Container(
+          margin: const EdgeInsets.all(7),
+          decoration: BoxDecoration(
+              color: appOrange.withOpacity(0.7), shape: BoxShape.circle),
+          child: IconButton(
+            onPressed: (() => Navigator.pop(context)),
+            icon: const Icon(Icons.arrow_back),
+            tooltip: MaterialLocalizations.of(context).backButtonTooltip,
+            splashRadius: 24,
+            color: white,
+            padding: EdgeInsets.zero,
+            alignment: Alignment.center,
+            iconSize: 24,
+            enableFeedback: true,
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        padding:
+            const EdgeInsets.only(left: 20, right: 20, top: 15, bottom: 15),
+      ),
+    );
+  }
+}
